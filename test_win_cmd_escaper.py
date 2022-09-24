9edc4f7acba6968ba66b7b07902e25903773e513
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-09-25 01:06:41 +0200

    Reformat python code

--- test_win_cmd_escaper.py
+++ test_win_cmd_escaper.py
@@ -1,7 +1,8 @@
 import unittest
 from win_cmd_escaper import escape_cmd_argument
 
+
 class Test(unittest.TestCase):
 
     def test_basic_call(self):
-      escape_cmd_argument("test string")
+        escape_cmd_argument("test string")
