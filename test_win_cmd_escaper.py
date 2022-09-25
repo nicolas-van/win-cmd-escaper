217ac7ee38deff1f59219d99166525bf33d433cc
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-09-25 15:48:05 +0200

    Added basic structure

---
+++ test_win_cmd_escaper.py
@@ -0,0 +1,7 @@
+import unittest
+from win_cmd_escaper import escape_cmd_argument
+
+class Test(unittest.TestCase):
+
+    def test_basic_call(self):
+      escape_cmd_argument("test string")
---
+++ win_cmd_escaper.py
@@ -0,0 +1,7 @@
+
+def escape_cmd_argument(str):
+  """
+  Escapes an argument for the CMD command in Windows.
+  """
+  # naive implementation for now
+  return str
