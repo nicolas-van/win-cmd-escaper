b6d0c5aa607b6a4cadaf7979ec4fe781ff0963c5
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-09-26 01:59:41 +0200

    Added powershell code

--- win_cmd_escaper.py
+++ win_cmd_escaper.py
@@ -36,3 +36,20 @@ def escape_cmd_argument_script(str):
         else:
             acc += c
     return f'"{acc}"'
+
+_pw_script_char_to_escape_sequence = {
+    "'": lambda *args: "''",
+}
+
+def escape_powershell_argument_script(str):
+    """
+    Escapes an argument for the CMD command in Windows.
+    """
+    acc = ""
+    for i in range(len(str)):
+        c = str[i]
+        if c in _pw_script_char_to_escape_sequence:
+            acc += _pw_script_char_to_escape_sequence[c](str[0:i], str[i+1:])
+        else:
+            acc += c
+    return f"'{acc}'"
