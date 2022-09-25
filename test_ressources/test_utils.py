4b973806bc2aabe7d8ae9433b8ad0f8f2ad0d5c4
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-09-26 01:59:13 +0200

    Removed powershell implementation

--- test_ressources/test_utils.py
+++ test_ressources/test_utils.py
@@ -46,26 +46,3 @@ def run_echoer_with_cmd_through_python_subprocess(str):
         return output_content
     finally:
         shutil.rmtree(tmp_folder)
-
-def run_echoer_with_powershell_through_script(str):
-    tmp_folder = tempfile.mkdtemp()
-    try:
-        # create the bat file
-        output_file_path = os.path.join(tmp_folder, "output.txt")
-        cmd_file_content = f"""
-python "{echo_py_path}" --file "{output_file_path}" -- {str}
-"""
-        cmd_file_name = os.path.join(tmp_folder, "echofile.ps1")
-        with open(cmd_file_name, 'w', encoding="utf-8") as f:
-            f.write(cmd_file_content)
-
-        # perform the call
-        subprocess.check_call(["powershell", cmd_file_name], stdout=subprocess.DEVNULL)
-
-        # get the results
-        with open(output_file_path, 'r', encoding="utf-8", newline='') as f:
-            output_content = f.read()
-
-        return output_content
-    finally:
-        shutil.rmtree(tmp_folder)
--- win_cmd_escaper.py
+++ win_cmd_escaper.py
@@ -36,20 +36,3 @@ def escape_cmd_argument_script(str):
         else:
             acc += c
     return f'"{acc}"'
-
-_pw_script_char_to_escape_sequence = {
-    "'": lambda *args: "''",
-}
-
-def escape_powershell_argument_script(str):
-    """
-    Escapes an argument for the CMD command in Windows.
-    """
-    acc = ""
-    for i in range(len(str)):
-        c = str[i]
-        if c in _pw_script_char_to_escape_sequence:
-            acc += _pw_script_char_to_escape_sequence[c](str[0:i], str[i+1:])
-        else:
-            acc += c
-    return f"'{acc}'"
