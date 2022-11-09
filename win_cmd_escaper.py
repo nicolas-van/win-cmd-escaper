17e0867e9ac78cdf00274b7ec1034f01fb7d81f0
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2022-11-10 00:24:32 +0100

    wip

--- win_cmd_escaper.py
+++ win_cmd_escaper.py
@@ -1,43 +1,35 @@
 
 from io import UnsupportedOperation
 
-
 def escape_cmd_argument_direct(str):
     """
     Escapes an argument for the CMD command in Windows.
+
+    This variant is the one to use when using direct system call invoking CMD on Windows. If
+    you are intending to integrate the result of this function in a .bat script you should use
+    `escape_cmd_argument_script()` instead.
     """
-    acc = ""
-    for i in range(len(str)):
-        c = str[i]
-        if ord(c) < 32:
-            raise UnsupportedOperation("ASCII control codes are not supported")
-        elif c == '"':
-            acc += '""'
-        elif c == "\\":
-            bs_esc = False
-            for j in range(i + 1, len(str) + 1):
-                if j == len(str) + 1 or str[j] == '"':
-                    bs_esc = True
-                    break
-                elif str[j] == "\\":
-                    continue
-                else:
-                    break
-            acc += "\\\\" if bs_esc else "\\"
-        else:
-            acc += c
-    return f'"{acc}"'
+
+    return __escape_cmd_common(str, False)
 
 def escape_cmd_argument_script(str):
     """
     Escapes an argument for the CMD command in Windows.
+
+    This variant is the one to use when you want to create a .bat script. If you are intending to
+    integrate the result of this function in direct system call to CMD you should use
+    `escape_cmd_argument_direct()` instead.
     """
+
+    return __escape_cmd_common(str, True)
+
+def __escape_cmd_common(str, is_script):
     acc = ""
     for i in range(len(str)):
         c = str[i]
         if ord(c) < 32:
             raise UnsupportedOperation("ASCII control codes are not supported")
-        elif c == "%":
+        elif is_script and c == "%":
             acc += "%%"
         elif c == '"':
             acc += '""'
@@ -58,10 +50,11 @@ def escape_cmd_argument_script(str):
 
 def escape_powershell_argument_script(str):
     """
-    Escapes an argument for the CMD command in Windows.
+    Escapes an argument for Powershell.
     """
     if str == "":
         raise UnsupportedOperation("Empty strings are not supported")
+    
     acc = ""
     for i in range(len(str)):
         c = str[i]
